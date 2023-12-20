4bbe20110612495d40780bdb018a6e66da36a0c1
Sunny Pradhan <sun.pradh@gmail.com>
2023-12-20 17:27:01 +0100

    mistype in script

--- sunny/compute_renyi_inf.py
+++ sunny/compute_renyi_inf.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.integrate as integrate
 
 from shannon import renyi_inf_entropy as rie
 from scipy.optimize import curve_fit
@@ -17,6 +18,18 @@ fit_fn = lambda x, a, b, c: a*x + b*np.log(x) + c
 
 fit_res_even = tuple(curve_fit(fit_fn, lens_even, smax_even)[0])
 fit_res_odd  = tuple(curve_fit(fit_fn, lens_odd,  smax_odd )[0])
+
+def print_fit_res(fit_res):
+    print(f"\t alpha = {fit_res[0]}")
+    print(f"\t beta  = {fit_res[1]}")
+    print(f"\t gamma = {fit_res[2]}")
+    print()
+
+
+expected_alpha = - integrate.quad(
+                        lambda x: np.log(np.sin(x)), 0, np.pi
+                    )[0] / (2 * np.pi)
+
 
 plt.subplots(1, 2)
 
@@ -39,13 +52,11 @@ plt.grid()
 plt.xlabel(r"$L$")
 plt.ylabel("difference")
 
-def print_fit_res(fit_res):
-    print(f"\t alpha = {fit_res[0]}")
-    print(f"\t beta  = {fit_res[1]}")
-    print(f"\t gamma = {fit_res[2]}")
-    print()
-
-print("Fit results for odd lengths:")
-print_fit_res(fit_res_even)
 print("Fit results for even lengths:")
+print_fit_res(fit_res_even)
+print("Fit results for odd lengths:")
 print_fit_res(fit_res_odd)
+
+print(f"Expected alpha: {expected_alpha}")
+
+plt.show()
